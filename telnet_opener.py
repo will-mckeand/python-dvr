8af09fbb9d4ec3c56bfb6d791cd8c02a39cc6fd4
Dmitry Ilyin <dima@doty.ru>
2021-03-25 08:16:24 +0300

    Make json for object

--- telnet_opener.py
+++ telnet_opener.py
@@ -3,11 +3,12 @@
 from dvrip import DVRIPCam
 import zipfile
 import socket
+import json
 
 
 def make_zip(filename, data):
-    zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-    zipf.writestr('InstallDesc', data)
+    zipf = zipfile.ZipFile(filename, "w", zipfile.ZIP_DEFLATED)
+    zipf.writestr("InstallDesc", data)
     zipf.close()
 
 
@@ -18,23 +19,20 @@ def check_port(host_ip, port):
 
 
 def open_telnet(host_ip):
-    install_desc = """{
-	"UpgradeCommand":	[{
-			"Command": "Shell",
-			"Script": "armbenv -s xmuart 0; armbenv -s telnetctrl 1"
-		}, {
-			"Command": "Shell",
-			"Script": "busybox telnetd -F -p 4321 -l /bin/sh"
-		}],
-  "Hardware": "53H20L_S39",
-  "DevID": "000025321001000000000000",
-  "CompatibleVersion": 2,
-  "Vendor": "General",
-  "CRC": "1ce6242100007636"
-}
-"""
-    zipfname = 'IPC_XiongMai_00000197_HI3520DV200_telnet_ZFTLAB-20210324.bin'
-    make_zip(zipfname, install_desc)
+    armbenv = {
+        "Command": "Shell",
+        "Script": "armbenv -s xmuart 0; armbenv -s telnetctrl 1",
+    }
+    telnetd = {
+        "Command": "Shell",
+        "Script": "busybox telnetd -F -p 4321 -l /bin/sh",
+    }
+    desc = {"UpgradeCommand": [armbenv, telnetd], 'Hardware': '53H20L_S39',
+            'DevID': '000025321001000000000000', 'CompatibleVersion': 2,
+            'Vendor': 'General', 'CRC': '1ce6242100007636'
+            }
+    zipfname = "IPC_XiongMai_00000197_HI3520DV200_telnet_ZFTLAB-20210324.bin"
+    make_zip(zipfname, json.dumps(desc, indent=2))
     cam = DVRIPCam(host_ip, user="admin", password="")
     if not cam.login():
         print(f"Cannot connect {host_ip}")
@@ -53,5 +51,5 @@ def main():
     open_telnet("IPG-53H20PL-S_2")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
